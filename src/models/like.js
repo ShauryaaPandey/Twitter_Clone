893475bc3ad0d764740821360ca0d5ae60d11222
Shaurya Pandey <shauryapandey40@gmail.com>
2026-04-18 00:05:41 +0530

    Added like model and basic user model

---
+++ src/models/like.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const LikeSchema = new mongoose.Schema({
+     onModel : {
+        type : String,
+        required : true,
+        enum : ['Tweet' , 'Comment'],
+     }, 
+     likeable : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        refPath: 'onModel'
+     },
+     user : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : 'User'
+     }
+} , {timestamsps : true});
+
+const Like = mongoose.model('Like',LikeSchema);
+
+export default Like;
